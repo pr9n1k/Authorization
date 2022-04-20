6d5efa375f0e1e5e716e7d31a3d73ec8c3e927d0
pr9n1k <74241094+pr9n1k@users.noreply.github.com>
2022-04-20 18:44:42 +0300

    init

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import './app.css';
+
+export function App() {
+  return (
+    <>
+      <h1>Старт</h1>
+    </>
+  );
+}
+
+export default App;
